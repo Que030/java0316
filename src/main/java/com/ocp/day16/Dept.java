8e51e8531360e9ca2b1717186572880cdc9aa18d
MB-study <MB-study@192.168.1.148>
2021-04-22 19:32:13 +0800

    030

--- src/main/java/com/ocp/day16/Dept.java
+++ src/main/java/com/ocp/day16/Dept.java
@@ -8,7 +8,7 @@ public static final String companyName = "HaHa";
         public void job(){
             String name = "寫程式";
             System.out.printf("%s 在 %s 公司的 %s 部門下 %s\n",
-                    this.name ,companyName ,Dept.this.name ,name);
+                    this.name ,Dept.companyName ,Dept.this.name ,name);
         }
         
     }
