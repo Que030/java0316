b8abe4c2bfb9acf5c303dc20b568c0e2c5e634e9
MB-study <MB-study@192.168.1.148>
2021-04-22 19:45:41 +0800

    030

--- src/main/java/com/ocp/day16/SevenEleven.java
+++ src/main/java/com/ocp/day16/SevenEleven.java
@@ -1,5 +1,24 @@
 package com.ocp.day16;
 
 public class SevenEleven {
-    
+//結帳方法
+    public void submit(int price) {
+        String name = "微粒炸醬麵";
+        
+//交易類別        
+        class Tansaotion {
+
+//交易資料            
+            void print() {
+                System.out.printf("交易金額 %d\n ", price);
+            }
+        }
+
+        Tansaotion t = new Tansaotion();
+        t.print();
+    }
+    public static void main(String[] args) {
+        SevenEleven se = new SevenEleven();
+        se.submit(100);
+    }
 }
